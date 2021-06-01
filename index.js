0884853c6340d50dc2c745750cbaee6938a22a51
Victoria <victoriathorsenn@gmail.com>
2021-06-01 18:05:11 -0400

    commit tree e0810ea62b5c592bca2d02a707130d2ed9d923db

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Your code here
+let dodger = document.querySelector('#dodger')
+dodger.style.backgroundColor = 'green'
+
+document.addEventListener('keydown', function(e){
+    if (e.key == "ArrowLeft") {
+        moveDodgerLeft();
+    } else if (e.key == "ArrowRight") {
+        moveDodgerRight();
+    }
+})
+
+function moveDodgerLeft(){
+    let leftNum = dodger.style.left.replace("px","")
+    let left = parseInt(leftNum, 10)
+    if (left > 0) {
+        dodger.style.left = `${left - 1}px`
+    }
+}
+
+function moveDodgerRight(){
+    let rightNum = dodger.style.left.replace("px","")
+    let right = parseInt(rightNum, 10)
+    if (right < 360) {
+        dodger.style.left = `${right + 1}px`
+    }
+}
